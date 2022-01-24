90790c098c4f05a7cc47e5cb28e9c6b7bcb89fe2
otter <a33397397@gmail.com>
2022-01-25 00:01:12 +0900

    2022 01 25

---
+++ others/한글/종성의 여부에 따른 조사 결정.kt
@@ -0,0 +1,31 @@
+//fun main() {
+//    val word = readLine()!!.toString()
+//
+//    //todo 단어의 뒤에 올 조사를 결정함
+//    println("${decidePostposition(word, "은", "는")} ~~~")
+//    println()
+//
+//    println("${decidePostposition("메타몽", "은", "는")} 몽몽메타몽.")
+//    println("${decidePostposition("피카츄", "은", "는")} 케첩마니아입니다.")
+//    println()
+//    println("${decidePostposition("포켓몬 미스터리 던전", "을", "를")} 플레이해주세요. 하늘의 탐험대 재밌어요.")
+//    println("${decidePostposition("특히 탐험대", "을", "를")} 해주세요. 구조대보다 재밌어요.")
+//    println()
+//    println("${decidePostposition("그란돈", "이", "가")} 공격합니다.")
+//    println("${decidePostposition("가이오가", "이", "가")} 공격합니다.")
+//
+//}
+//
+//fun decidePostposition(word: String, existCoda: String, notExistCoda: String): String  {
+//    val lastChar: Char = word[word.length - 1]
+//
+//    // 한글의 시작(가)이나 끝(힣) 범위 초과시 오류
+//    if(lastChar.code < 0xAC00 || lastChar.code > 0xD7A3) {
+//        return word
+//    }
+//    val postposition: String =
+//        if ((lastChar.code - 0xAC00) % 28 > 0) existCoda
+//        else notExistCoda
+//
+//    return word+postposition
+//}
---
+++ others/한글/초성 중성 종성 나누기.kt
@@ -0,0 +1,30 @@
+//fun main() {
+//    val word = readLine()!!.toString()
+//
+//    print(segmentKorean(word))
+//}
+//
+//fun segmentKorean(word: String): MutableList<Char>  {
+//    //todo 글자를 초성, 중성, 종성으로 나눈다.
+//    val segmentedKorean: MutableList<Char> = mutableListOf()
+//
+//    for(i in word.indices) {
+//        val word2 = word[i].code - 0xAC00
+//        val onset = word2 / (21 * 28)
+//        val nucleus = word2 % (21 * 28) / 28
+//        val coda = word2 % 28
+//
+//        println("초성: $onset → ${(0x1100 + onset).toChar()}")
+//        segmentedKorean.add((0x1100 + onset).toChar())
+//        println("중성: $nucleus → ${(0x1161 + nucleus).toChar()}")
+//        segmentedKorean.add((0x1161 + nucleus).toChar())
+//        if(coda > 0) {
+//            println("종성: $coda → ${(0x11A8 + coda - 1).toChar()}")
+//            segmentedKorean.add((0x11A8 + coda - 1).toChar())
+//        }
+//        println()
+//        segmentedKorean.add(' ')
+//    }
+//
+//    return segmentedKorean
+//}
